859dbd633db62aa7c5d48617531d21ff3d187cc6
Ziiyodullayevv <akobirziyod2323@gmail.com>
2025-05-09 21:20:26 +0500

    MainNavigation component update

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -9,73 +9,75 @@ export default function MainNavigation() {
   const navigate = useNavigate();
   const { t } = useTranslation('common');
   return (
-    <header className='section-container sticky top-0 bg-white z-999 border-b'>
-      <div className='flex xl:text-sm justify-between py-3 items-center'>
-        <nav className='flex gap-5 xl:gap-7 items-center'>
-          <Logo />
-          <div className='h-full hidden md:flex items-center xl:hidden'>
+    <header className='h-18 sticky bg-white top-0 z-50 border-b'>
+      <div className='section-container h-full'>
+        <div className='flex xl:text-sm h-full justify-between items-center'>
+          <nav className='flex gap-5 h-full xl:gap-7 items-center'>
+            <Logo />
+            <div className='hidden md:flex items-center xl:hidden'>
+              <MobileMenu />
+            </div>
+            <ul className='hidden h-full xl:flex gap-4 items-center text-lg'>
+              <li className='h-full'>
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'flex justify-center items-center border-b border-black h-full'
+                      : 'h-full flex justify-center hover:text-v6 transition-colors duration-100 items-center'
+                  }
+                  to={'/about'}
+                >
+                  {t('about')}
+                </NavLink>
+              </li>
+              <li className='h-full'>
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'flex justify-center items-center border-b border-black h-full'
+                      : 'h-full flex justify-center items-center transition-colors duration-100 hover:text-v6'
+                  }
+                  to={'/services'}
+                >
+                  {t('services')}
+                </NavLink>
+              </li>
+              <li className='h-full'>
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'flex justify-center items-center border-b border-black h-full'
+                      : 'h-full flex justify-center hover:text-v6 transition-colors duration-100 items-center'
+                  }
+                  to={'/usecases'}
+                >
+                  {t('useCases')}
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <div className='hidden md:flex justify-start gap-3'>
+            <Button
+              onClick={() => navigate('auth')}
+              className='h-12 rounded-xl bg-transparent text-black shadow-none hover:bg-transparent text-base px-2 cursor-pointer'
+            >
+              Log in
+            </Button>
+
+            <Button
+              onClick={() => navigate('auth')}
+              className='h-12 rounded-xl text-base px-5 cursor-pointer'
+            >
+              Sign up
+            </Button>
+            <LanguageSwitcher />
+          </div>
+
+          <div className='h-full gap-4 flex md:hidden items-center xl:hidden'>
+            <LanguageSwitcher />
             <MobileMenu />
           </div>
-          <ul className='hidden xl:flex items-center text-lg'>
-            <li>
-              <NavLink
-                className={({ isActive }) =>
-                  isActive
-                    ? 'bg-v2 flex justify-center items-center rounded-lg h-12 px-3'
-                    : 'h-12 px-3'
-                }
-                to={'/about'}
-              >
-                {t('about')}
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                className={({ isActive }) =>
-                  isActive
-                    ? 'bg-v2 flex justify-center items-center rounded-lg h-12 px-3'
-                    : 'h-12 px-3'
-                }
-                to={'/services'}
-              >
-                {t('services')}
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                className={({ isActive }) =>
-                  isActive
-                    ? 'bg-v2 flex  justify-center items-center rounded-lg h-12 px-3'
-                    : 'h-12 px-3'
-                }
-                to={'/usecases'}
-              >
-                {t('useCases')}
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-
-        <div className='hidden md:flex justify-start gap-3'>
-          <Button
-            onClick={() => navigate('auth')}
-            className='h-12 rounded-xl bg-transparent text-black shadow-none hover:bg-transparent text-base px-2 cursor-pointer'
-          >
-            Log in
-          </Button>
-
-          <Button
-            onClick={() => navigate('auth')}
-            className='h-12 rounded-xl text-base px-5 cursor-pointer'
-          >
-            Sign up
-          </Button>
-          <LanguageSwitcher />
-        </div>
-
-        <div className='h-full gap-4 flex md:hidden items-center xl:hidden'>
-          <LanguageSwitcher />
-          <MobileMenu />
         </div>
       </div>
     </header>
