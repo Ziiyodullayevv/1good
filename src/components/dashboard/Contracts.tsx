6daa360993f5fded54eb8e8c9559946870b45063
Ziiyodullayevv <akobirziyod2323@gmail.com>
2025-07-13 15:36:27 +0500

    bug fixed

--- src/components/dashboard/Contracts.tsx
+++ src/components/dashboard/Contracts.tsx
@@ -1,40 +1,13 @@
 import Banner from './Banner';
 import CustomTable from './CustomTable';
 
-const data = [
-  {
-    id: '1',
-    title: 'Website Redesign for Local Bakery',
-    clientName: 'Sarah Johnson',
-    deadline: '2024-08-15',
-    status: 'In Progress',
-    action: 'View Details',
-  },
-  {
-    id: '2',
-    title: 'Mobile App Development for Fitness Tracker',
-    clientName: 'David Lee',
-    deadline: '2024-08-15',
-    status: 'In Progress',
-    action: 'View Details',
-  },
-  {
-    id: '3',
-    title: 'Content Marketing Strategy for E-commerce Store',
-    clientName: 'Emily Chen',
-    deadline: '2024-08-15',
-    status: 'In Progress',
-    action: 'View Details',
-  },
-];
-
 export default function Contracts() {
   return (
     <div className='bg-white min-h-[calc(100vh-72px)] sm:min-h-[calc(100vh-80px)] text-base rounded-xl overflow-hidden'>
       <Banner title='Contracts' />
 
       <div className='p-0 sm:p-8'>
-        <CustomTable data={data} />
+        <CustomTable />
       </div>
     </div>
   );
